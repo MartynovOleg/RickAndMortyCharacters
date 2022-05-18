517f6af95357d465c4fd6669b48f72863cd886d9
MartynovOleg <103255306+MartynovOleg@users.noreply.github.com>
2022-05-18 16:09:12 +0300

    added tableView

--- NoStoryboard/ViewController.swift
+++ NoStoryboard/ViewController.swift
@@ -7,11 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var tableView: UITableView {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.reloadData()
+        return table
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Таблица"
+        view.backgroundColor = .systemBlue
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        100
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemOrange
+//        cell.didAddSubview(UIView.init(frame: CGRect.init(x: 0, y: 0, width: 50, height: 100)))
+        return cell
     }
 
 
